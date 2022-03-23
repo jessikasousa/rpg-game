b4153da62dfcac2a667629ab1a10f2e497c680c0
Jéssika Paola de Sousa <jessika.sousa@conveste.com.br>
2022-03-23 17:58:26 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using rpg_game.src.Entities;
+
+namespace dotnet__poo 
+{
+   class Program
+   {
+       static void Main(string[] args) 
+       {
+           Hero hero = new Hero("Arus", 23, "Knight");
+           Wizard wizard = new Wizard("Jenica", 24, "White Wizard");
+
+           Console.WriteLine(wizard.Attack());
+       }
+   } 
+}
